7fa89d469a1b840a93208e9d2ba770cbbca7dce8
Jinyoung Yoo <1.jinyoung.yoo@gmail.com>
2019-03-20 18:10:07 +0900

    Connect graphql server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 const express = require('express');
+const ApolloServer = require('apollo-server-express');
 const mongoose = require('../db/mongoose');
 const router = require('./routes/index');
+const server = require('./graphql/index');
 const morgan = require('morgan');
 
 const app = express();
@@ -17,6 +19,6 @@ if (process.env.NODE_ENV == 'production') {
 app.use(express.json());
 app.use(morgan('combined'));
 app.use('/api', router);
-
+server.applyMiddleware({ app });
 app.listen(3000);
 console.log('Server running at http://localhost:3000');
